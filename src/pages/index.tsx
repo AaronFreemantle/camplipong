bb6b63737c51b292e0d079c1ee6045fb4beaefa2
AaronFreemantle <muso96@gmail.com>
2023-05-02 18:51:11 +1000

    Create match mutation and private procedure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,50 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
     const user = useUser();
 
-    const { data } = api.match.getAll.useQuery();
     return (
         <>
             <Head>
                 <title>Camplipong</title>
-                <meta
-                    name="description"
-                    content="Camplify Ping Pong Leaderboard"
-                />
+                <meta name="description" content="Camplify Ping Pong Leaderboard" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div>{!user.isSignedIn ? <SignInButton /> : <SignOutButton />}</div>
-            {user.isSignedIn && <RecordMatch />}
-            <div>
-                {data?.map((match) => (
-                    <div key={match.id}>
-                        {match.playerOneId} {match.playerTwoId}
-                    </div>
-                ))}
-            </div>
+            {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
+            {user.isSignedIn && <AddMatch />}
+            <MatchList />
         </>
     );
 };
 
-const RecordMatch = () => {
-    const { user } = useUser();
+const AddMatch = () => {
     const { data: users } = api.user.getAll.useQuery();
+    const { mutate: createMatch } = api.match.create.useMutation();
 
-    if (!user) return null;
+    const [opponent, setOpponent] = useState("");
+    const [playerOneScore, setPlayerOneScore] = useState<number>();
+    const [playerTwoScore, setPlayerTwoScore] = useState<number>();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!opponent || !playerOneScore || !playerTwoScore) return;
+        createMatch({
+            playerTwoId: opponent,
+            playerOneScore,
+            playerTwoScore,
+        });
+    };
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Opponent</label>
-                <select placeholder="Select an Opponent">
-                    <option value="" disabled selected>
+                <select defaultValue="" placeholder="Select an Opponent" onChange={(e) => setOpponent(e.target.value)}>
+                    <option value="" disabled>
                         Select an Opponent
                     </option>
                     {users?.map((user) => (
@@ -51,11 +54,26 @@ const RecordMatch = () => {
                     ))}
                 </select>
                 <label>Your Score</label>
-                <input />
+                <input type="number" inputMode="numeric" onChange={(e) => setPlayerOneScore(+e.target.value)} />
                 <label>{`Opponent's Score`}</label>
-                <input />
+                <input type="number" inputMode="numeric" onChange={(e) => setPlayerTwoScore(+e.target.value)} />
+                <input type="submit" value="Submit" />
             </form>
         </div>
+    );
+};
+
+const MatchList = () => {
+    const { data: matches } = api.match.getAll.useQuery();
+    return (
+        <>
+            {matches?.map((match) => (
+                <div key={match.id}>
+                    {match.playerOneId} {match.playerOneScore}
+                    {match.playerTwoId} {match.playerTwoScore}
+                </div>
+            ))}
+        </>
     );
 };
 
--- src/server/api/routers/match.ts
+++ src/server/api/routers/match.ts
@@ -1,9 +1,31 @@
 import { z } from "zod";
-
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
 export const matchRouter = createTRPCRouter({
     getAll: publicProcedure.query(({ ctx }) => {
         return ctx.prisma.match.findMany();
     }),
+
+    create: privateProcedure
+        .input(
+            z.object({
+                playerTwoId: z.string(),
+                playerOneScore: z.number(),
+                playerTwoScore: z.number(),
+                ranked: z.boolean().default(false),
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const { session } = ctx;
+
+            console.log("Match created: ", input);
+            const match = await ctx.prisma.match.create({
+                data: {
+                    playerOneId: session.userId,
+                    ...input,
+                },
+            });
+
+            return match;
+        }),
 });
