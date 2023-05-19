2ee2b47609fd2a27249c9d11f47b437d4137f5ed
AaronFreemantle <muso96@gmail.com>
2023-05-19 13:47:50 +1000

    Only show elo change for ranked matches

--- src/components/MatchList.tsx
+++ src/components/MatchList.tsx
@@ -44,8 +44,8 @@ const MatchRow = ({ match, playerOne, playerTwo }: { match: Match; playerOne: Us
         <TableRow className="bg-background">
             <TableCell className="p-1 text-center">
                 {!match.ranked && <p>-</p>}
-                {match.playerOneDiff >= 0 && <p className="text-green-500">+{match.playerOneDiff}</p>}
-                {match.playerOneDiff < 0 && <p className="text-red-500">{match.playerOneDiff}</p>}
+                {match.ranked && match.playerOneDiff >= 0 && <p className="text-green-500">+{match.playerOneDiff}</p>}
+                {match.ranked && match.playerOneDiff < 0 && <p className="text-red-500">{match.playerOneDiff}</p>}
             </TableCell>
             <TableCell className="flex items-center justify-start gap-2 p-0">
                 <Avatar className="h-8 w-8">
@@ -74,8 +74,8 @@ const MatchRow = ({ match, playerOne, playerTwo }: { match: Match; playerOne: Us
             </TableCell>
             <TableCell className="p-1 text-center">
                 {!match.ranked && <p>-</p>}
-                {match.playerTwoDiff >= 0 && <p className="text-green-500">+{match.playerTwoDiff}</p>}
-                {match.playerTwoDiff < 0 && <p className="text-red-500">{match.playerTwoDiff}</p>}
+                {match.ranked && match.playerTwoDiff >= 0 && <p className="text-green-500">+{match.playerTwoDiff}</p>}
+                {match.ranked && match.playerTwoDiff < 0 && <p className="text-red-500">{match.playerTwoDiff}</p>}
             </TableCell>
             <TableCell className="p-1">
                 <p>{match.ranked ? "Ranked" : "Casual"}</p>
