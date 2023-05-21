e5e55fb58d623cc975ddfad8f740d8c0ec5e54d9
AaronFreemantle <muso96@gmail.com>
2023-05-21 19:14:57 +1000

    Fix dark mode color transitions

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -34,7 +34,7 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
     ({ className, ...props }, ref) => (
         <tr
             ref={ref}
-            className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)}
+            className={cn("border-b hover:bg-muted/50 data-[state=selected]:bg-muted", className)}
             {...props}
         />
     )
@@ -46,7 +46,7 @@ const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<
         <th
             ref={ref}
             className={cn(
-                "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+                "text-muted-foreground h-12 px-4 text-left align-middle font-medium [&:has([role=checkbox])]:pr-0",
                 className
             )}
             {...props}
@@ -64,7 +64,7 @@ TableCell.displayName = "TableCell";
 
 const TableCaption = React.forwardRef<HTMLTableCaptionElement, React.HTMLAttributes<HTMLTableCaptionElement>>(
     ({ className, ...props }, ref) => (
-        <caption ref={ref} className={cn("mt-4 text-sm text-muted-foreground", className)} {...props} />
+        <caption ref={ref} className={cn("text-muted-foreground mt-4 text-sm", className)} {...props} />
     )
 );
 TableCaption.displayName = "TableCaption";
