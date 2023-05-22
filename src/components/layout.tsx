9ab2994eaa913759baf76800c6219dcb4ef7aeec
AaronFreemantle <muso96@gmail.com>
2023-05-22 21:58:58 +1000

    Gradient bad

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,10 +3,10 @@ import Navbar from "./navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="bg-gradient-to-tr from-crust to-surface0">
+        <>
             <Navbar />
             <main className="m-auto max-w-screen-xl">{children}</main>
             <Footer />
-        </div>
+        </>
     );
 }
